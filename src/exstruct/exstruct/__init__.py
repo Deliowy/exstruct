fae60ac1dfbbc3dec2b02d4040f2de1fddbd5f92
Kirill Sukhorukov <91054546+Deliowy@users.noreply.github.com>
2023-02-16 16:11:33 +0300

    Changed package layout to follow pytest examples

---
+++ src/exstruct/exstruct/__init__.py
@@ -0,0 +1,14 @@
+from ._base_exstruct import BaseExStruct
+from ._csv_exstruct import CSVExStruct
+from ._json_exstruct import JSONExStruct, JSONSchemaExStruct
+from ._xml_exstruct import XMLExStruct
+from ._xsd_exstruct import XSDExStruct
+
+__all__ = [
+    "BaseExStruct",
+    "CSVExStruct",
+    "JSONExStruct",
+    "JSONSchemaExStruct",
+    "XMLExStruct",
+    "XSDExStruct",
+]
