a68f14aabb7cfa4101e3fc4919bdc3292c868dda
Kirill Sukhorukov <91054546+Deliowy@users.noreply.github.com>
2023-03-20 16:28:16 +0300

    fix for extractor

--- src/exstruct/extractor/_extractor.py
+++ src/exstruct/extractor/_extractor.py
@@ -1,11 +1,20 @@
+import concurrent.futures
+import json
+import pathlib
+import tempfile
 import typing
+from concurrent.futures import ThreadPoolExecutor
 
 import dateutil
 import dateutil.parser
+import h5py
+import numpy
 
 from ..util import _util
 
 logger = _util.getLogger("exstruct.extractor.extractor")
+
+DEFAULT_BATCH_SIZE = 10_000
 
 
 # DONE TODO IMPROVE
@@ -17,32 +26,91 @@ class DataExtractor(object):
         mapping: dict = None,
         mapping_delimiter: str = " -> ",
         root_prefix: str = None,
+        *args,
+        **kwargs,
     ) -> None:
         self._mapping = mapping
         self._mapping_delimiter = mapping_delimiter
         self._root_prefix = root_prefix
+        self._batch_size = kwargs.pop("batch_size", DEFAULT_BATCH_SIZE)
+
+    def extract_from_file(self, filename: str):
+        extracted_data_file = tempfile.TemporaryFile(
+            dir=pathlib.Path(filename).parent, delete=False, suffix=".hdf5"
+        )
+        extracted_data_hdf5 = h5py.File(extracted_data_file, "r+")
+        extracted_data_dset = extracted_data_hdf5.create_dataset(
+            "extracted_data",
+            dtype=h5py.string_dtype("utf-8"),
+            shape=(0,),
+            maxshape=(None,),
+            compression="gzip",
+            chunks=(self._batch_size,),
+            shuffle=True,
+        )
+
+        file = h5py.File(filename)
+        for key in file.keys():
+            dset = file[key]
+            batch = []
+            processed_data = 0
+            with ThreadPoolExecutor(max_workers=100) as executor:
+                futures_to_extracted_data = [
+                    executor.submit(self.extract, pre_extracted_data)
+                    for pre_extracted_data in map(json.loads, dset)
+                ]
+
+            for future in concurrent.futures.as_completed(futures_to_extracted_data):
+                extracted_data = future.result()
+                batch.append(
+                    json.dumps(extracted_data, ensure_ascii=False, default=str)
+                )
+                if len(batch) > self._batch_size:
+                    extracted_data_dset.resize((processed_data + self._batch_size,))
+                    extracted_data_dset[processed_data:] = numpy.asarray(
+                        batch[: self._batch_size + 1]
+                    )
+                    processed_data += self._batch_size + 1
+                    extracted_data_dset.flush()
+                    del batch[: self._batch_size + 1]
+
+            if batch:
+                extracted_data_dset.resize((processed_data + len(batch),))
+                extracted_data_dset[processed_data:] = numpy.asarray(batch)
+                extracted_data_dset.flush()
+                processed_data += len(batch)
+                del batch
+
+        file.close()
+        extracted_data_hdf5.flush()
+        extracted_data_hdf5.close()
+        return extracted_data_file
 
     def extract(self, content: dict | typing.Iterable[dict]):
         if isinstance(content, dict) and len(content) > 1:
             err_msg = "Data type elements must be incapsulated in a dict"
             raise ValueError(err_msg)
 
-        if isinstance(content, (list, tuple)):
+        if isinstance(content, (list, tuple, map)):
             return map(self.extract, content)
 
         data_container = content[self.root_prefix] if self.root_prefix else content
         processed_data_type_name = list(data_container.keys()).pop()
 
         processed_data_type = self.mapping[processed_data_type_name]
+        processed_data_type_settings = processed_data_type["@collected_info"]
+        data_type_mapping_name = self._data_type_mapping_name(
+            processed_data_type_settings
+        )
 
         extracted_data = {}
 
-        if processed_data_type["@collected_info"]["collected_info_type"] == "V":
-            extracted_data[processed_data_type_name] = self._extract_data(
+        if processed_data_type_settings["collected_info_type"] == "V":
+            extracted_data[data_type_mapping_name] = self._extract_data(
                 data_container[processed_data_type_name], processed_data_type
             )
-        elif processed_data_type["@collected_info"]["collected_info_type"] == "E":
-            extracted_data[processed_data_type_name] = (
+        elif processed_data_type_settings["collected_info_type"] == "E":
+            extracted_data[data_type_mapping_name] = (
                 True if data_container[processed_data_type_name] else False
             )
         else:
@@ -54,7 +122,11 @@ class DataExtractor(object):
     def _extract_data(self, content: dict, data_type_mapping: dict):
         data_type_settings = data_type_mapping["@collected_info"]
         data_type_elements = dict(
-            [item for item in data_type_mapping.items() if not item[0].startswith("@")]
+            [
+                item
+                for item in data_type_mapping.items()
+                if not item[0].startswith("@collected_info")
+            ]
         )
 
         if data_type_settings["collected_info_type"] == None:
@@ -72,7 +144,9 @@ class DataExtractor(object):
                     for item in content:
                         extracted_data.append(
                             {
-                                key: self._extract_data(
+                                self._data_type_mapping_name(
+                                    data_type_elements[key]["@collected_info"]
+                                ): self._extract_data(
                                     item.get(key, None),
                                     data_type_elements[key],
                                 )
@@ -82,7 +156,11 @@ class DataExtractor(object):
                 else:
                     extracted_data = {}
                     for key in data_type_elements:
-                        extracted_data[key] = self._extract_data(
+                        extracted_data[
+                            self._data_type_mapping_name(
+                                data_type_elements[key]["@collected_info"]
+                            )
+                        ] = self._extract_data(
                             content.get(key, None),
                             data_type_elements[key],
                         )
@@ -97,6 +175,9 @@ class DataExtractor(object):
 
         elif data_type_mapping["collected_info_type"] == "E":
             return False if content is None else True
+
+    def _data_type_mapping_name(self, data_type_settings: dict):
+        return data_type_settings["mapping"].split(self.mapping_delimiter)[-1]
 
     # TODO Improve
     def _cast_data_value_to_type(self, data: str, data_type: str):
